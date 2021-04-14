03785293d2f3a0571101ffd0d42299a3042b4e3c
Neiell Care L. Paradiang <koneb2013@gmail.com>
2021-04-15 02:11:08 +0800

    adjustments

--- appsched/settings.py
+++ appsched/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-d307v4!v!@p1eidq&$0wj*i-*gt&illy&n6lb3m5a7tq9_8&x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
